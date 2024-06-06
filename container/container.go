ac33284acf12bc39280e6cd8d63b71de8b8babe9
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2024-06-06 14:44:35 +0700

    feat(container): use Get function to retrieve object

--- container/container.go
+++ container/container.go
@@ -2,29 +2,46 @@ package dhasar_container
 
 import (
 	"fmt"
-	"reflect"
+	"sync"
 )
 
 type Container struct {
-	dependencies map[reflect.Type]interface{}
+	mu           sync.RWMutex
+	dependencies map[string]interface{}
 }
 
-func New() *Container {
+func NewContainer() *Container {
 	return &Container{
-		dependencies: make(map[reflect.Type]interface{}),
+		dependencies: make(map[string]interface{}),
 	}
 }
 
-func (c *Container) Register(dependency interface{}) {
-	t := reflect.TypeOf(dependency)
-	c.dependencies[t] = dependency
+func (c *Container) Register(name string, dependency interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.dependencies[name] = dependency
 }
 
-func Get[T any](c *Container) T {
-	t := reflect.TypeOf((*T)(nil)).Elem()
-	dependency, exists := c.dependencies[t]
-	if !exists {
-		panic(fmt.Sprintf("no dependency found for type %v", t))
+func (c *Container) Resolve(name string) (interface{}, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	dependency, ok := c.dependencies[name]
+	if !ok {
+		return nil, fmt.Errorf("dependency %s not found", name)
 	}
-	return dependency.(T)
+	return dependency, nil
+}
+
+func Get[T any](c *Container, name string) (T) {
+	dependency, err := c.Resolve(name)
+	if err != nil {
+    panic(fmt.Sprintf("%s is not resolved.", name))
+	}
+
+	dep, ok := dependency.(T)
+	if !ok {
+    panic(fmt.Sprintf("%s is not resolved.", name))
+	}
+
+	return dep
 }
